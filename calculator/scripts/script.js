4e574010d537f3d2b31bc362960c87f68f65a50d
AntonZelk <zelkdar@gmail.com>
2021-03-03 22:13:17 +0300

    commit2

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -1,8 +1,64 @@
-let cursor = document.querySelector("#cursor");
+const cursor = document.querySelector("#cursor"),
+  btns = document.querySelectorAll(".button"),
+  newMenu = document.querySelector("#newBtn"),
+  btnHistory = document.querySelector("#history"),
+  otherBtns = document.querySelectorAll(".but");
 
-let timer = setTimeout(() => {
-  cursor.style.display = "block";
-  let tick = setInterval(() => {
+const allBtn = [...btns, ...otherBtns];
+
+const tick = () => {
+  if (cursor.style.display == "block") {
     cursor.style.display = "none";
-  }, 1000);
-}, 1000);
+  } else {
+    cursor.style.display = "block";
+  }
+};
+
+const timer = setInterval(tick, 500);
+
+let changeBtns = (btn) => {
+  btn.forEach((el) => {
+    el.addEventListener("click", () => {
+      btn.forEach((el) => {
+        el.style.borderColor = "rgb(117, 115, 115)";
+      });
+      el.style.borderColor = "rgb(117, 228, 132)";
+    });
+  });
+};
+
+let noneBtns = (click, btns, otherBtns, flex, none) => {
+  let flag = true;
+  click.addEventListener("click", () => {
+    if (flag) {
+      otherBtns.forEach((el) => {
+        el.style.display = flex;
+        btns.forEach((el) => {
+          el.style.display = none;
+          el.style.borderColor = "rgb(117, 115, 115)";
+        });
+      });
+      flag = false;
+    } else {
+      btns.forEach((el) => {
+        el.style.display = flex;
+        otherBtns.forEach((el) => {
+          el.style.display = none;
+          el.style.borderColor = "rgb(117, 115, 115)";
+        });
+      });
+      flag = true;
+    }
+  });
+};
+
+let funcHistory = (btn) => {
+  btn.forEach((el) => {
+    el.style.display = "none";
+  });
+};
+changeBtns(allBtn);
+noneBtns(newMenu, btns, otherBtns, "flex", "none");
+btnHistory.addEventListener("click", () => {
+  funcHistory(allBtn);
+});
